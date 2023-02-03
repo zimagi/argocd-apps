f9d7be8880a08ed26c73822fa646f4d5ed846856
Adrian Webb <adrian@webb.sh>
2023-02-02 22:39:02 -0500

    Fixing concatenation issue in the locals manifest.

--- locals.tf
+++ locals.tf
@@ -10,7 +10,7 @@ locals {
 
 module "variables" {
   source = "Invicton-Labs/deepmerge/null"
-  maps   = concat(local.core_variables, var.variables)
+  maps   = [local.core_variables, var.variables]
 }
 locals {
   variables = module.variables.merged
