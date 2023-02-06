75995a05501fb4afa3419f17334b719ee9e8dafb
Adrian Webb <adrian@webb.sh>
2023-02-06 07:36:13 -0500

    Adding a nonsensitive function call in the release submodule locals manifest.

--- release/locals.tf
+++ release/locals.tf
@@ -6,7 +6,7 @@ locals {
     for file in fileset(var.config_path, "${var.group}/*.yaml") :
     yamldecode(templatefile("${var.config_path}/${file}", var.variables))
   ]
-  application_index = {
+  application_index = nonsensitive({
     for config in local.applications : config.name => config
-  }
+  })
 }
