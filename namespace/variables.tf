d948f1d1009b8e09ae0621906de298a0a804c6b9
Adrian Webb <adrian@webb.sh>
2023-02-06 06:24:12 -0500

    Adding default values for annotations, labels, secrets, and config maps in the namspace submodule.

--- namespace/variables.tf
+++ namespace/variables.tf
@@ -7,17 +7,21 @@ variable "name" {
 variable "labels" {
   description = "Kubernetes namespace labels"
   type        = map(any)
+  default     = {}
 }
 variable "annotations" {
   description = "Kubernetes namespace annotations"
   type        = map(any)
+  default     = {}
 }
 
 variable "secrets" {
   description = "Collection of namespaced secrets to manage for cluster (outside of values files)"
   type        = any
+  default     = {}
 }
 variable "config" {
   description = "Collection of namespaced config maps to manage for cluster"
   type        = any
+  default     = {}
 }
