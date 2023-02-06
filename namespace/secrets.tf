914686156d25c1aee2bffab6aa0285645dcd1979
Adrian Webb <adrian@webb.sh>
2023-02-06 07:06:54 -0500

    Updating the secrets for_each processing.

--- namespace/secrets.tf
+++ namespace/secrets.tf
@@ -1,18 +1,18 @@
 
 resource "kubernetes_secret" "secrets" {
-  for_each = var.secrets
+  for_each = toset([ for name, config in var.secrets : name ])
 
   metadata {
-    name        = each.key
+    name        = each.value
     namespace   = var.name
-    labels      = lookup(each.value, "labels", {})
-    annotations = lookup(each.value, "annotations", {})
+    labels      = lookup(var.secrets[each.value], "labels", {})
+    annotations = lookup(var.secrets[each.value], "annotations", {})
   }
 
-  type        = lookup(each.value, "type", "Opaque")
-  immutable   = lookup(each.value, "immutable", false)
-  data        = lookup(each.value, "data", {})
-  binary_data = lookup(each.value, "binary", {})
+  type        = lookup(var.secrets[each.value], "type", "Opaque")
+  immutable   = lookup(var.secrets[each.value], "immutable", false)
+  data        = lookup(var.secrets[each.value], "data", {})
+  binary_data = lookup(var.secrets[each.value], "binary", {})
 
   depends_on = [
     kubernetes_namespace.this
