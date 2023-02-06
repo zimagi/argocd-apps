a994eb4f228e87e753467fe53e414c24a37f34cf
Adrian Webb <adrian@webb.sh>
2023-02-06 09:47:24 -0500

    Moving all release kubernetes manifests into locals and updating project to match applications architecture.

--- release/locals.tf
+++ release/locals.tf
@@ -1,4 +1,7 @@
 
+#
+# Configuration and application data
+#
 locals {
   values_path = "${var.config_path}/${var.values_directory}"
 
@@ -9,4 +12,103 @@ locals {
   application_index = nonsensitive({
     for config in local.applications : config.name => config
   })
+}
+
+#
+# Kubernetes manifests
+#
+locals {
+  #
+  # ArgoCD Project
+  #
+  project_manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "AppProject"
+
+    metadata = {
+      name        = var.group
+      namespace   = var.argocd_namespace
+      labels      = var.labels
+      annotations = var.annotations
+      finalizers  = var.cascade_delete ? ["resources-finalizer.argocd.argoproj.io"] : []
+    }
+    spec = {
+      roles : [
+        for permission in var.permissions : {
+          name : permission["name"],
+          description : permission["description"],
+          policies : [
+            for policy in permission["policies"] :
+            "p, proj:${var.group}:${permission["name"]}, ${policy["resource"]}, ${policy["action"]}, ${var.group}/${policy["object"]}, allow"
+          ],
+          groups : permission["groups"]
+        }
+      ]
+    }
+  }
+
+  #
+  # ArgoCD Application
+  #
+  application_manifests = {
+    for name, config in local.application_index :
+      name => {
+        apiVersion = "argoproj.io/v1alpha1"
+        kind       = "Application"
+
+        metadata = {
+          name        = name
+          namespace   = var.argocd_namespace
+          labels      = merge(var.labels, lookup(config, "labels", {}))
+          annotations = merge(var.annotations, lookup(config, "annotations", {}))
+          finalizers  = var.cascade_delete == true ? ["resources-finalizer.argocd.argoproj.io"] : []
+        }
+        spec = {
+          project              = var.group
+          ignoreDifferences    = lookup(config, "ignore_differences", var.ignore_differences)
+          revisionHistoryLimit = lookup(config, "revision_history_limit", var.revision_history_limit)
+
+          syncPolicy = {
+            automated = {
+              prune      = lookup(config, "prune", var.prune)
+              selfHeal   = lookup(config, "self_heal", var.self_heal)
+              allowEmpty = lookup(config, "allow_empty", var.allow_empty)
+            }
+            syncOptions = concat(lookup(config, "sync_options", var.sync_options), [
+              lookup(config, "sync_validate", var.sync_validate) ? "Validate=true" : "Validate=false",
+              "CreateNamespace=false"
+            ])
+            retry = {
+              limit = lookup(config, "retry_limit", var.retry_limit)
+              backoff = {
+                duration    = lookup(config, "retry_backoff_duration", var.retry_backoff_duration)
+                factor      = lookup(config, "retry_backoff_factor", var.retry_backoff_factor)
+                maxDuration = lookup(config, "retry_backoff_max_duration", var.retry_backoff_max_duration)
+              }
+            }
+          }
+
+          source = {
+            repoURL        = lookup(config, "repository", var.default_repository)
+            chart          = lookup(config, "chart", var.default_chart) # Helm Repository only
+            path           = lookup(config, "path", var.default_path)  # Git Repository only
+            targetRevision = lookup(config, "version", var.default_version)
+
+            helm = {
+              releaseName     = lookup(config, "release", name)
+              passCredentials = lookup(config, "pass_credentials", false)
+              values          = fileexists("${local.values_path}/${name}.yaml") ? templatefile(
+                "${local.values_path}/${name}.yaml",
+                var.variables
+              ) : ""
+            }
+          }
+
+          destination = {
+            server    = lookup(config, "destination_server", var.destination_server)
+            namespace = var.group
+          }
+        }
+      }
+  }
 }
--- release/project.tf
+++ release/project.tf
@@ -5,31 +5,10 @@ resource "kubectl_manifest" "project" {
   wait_for_rollout = true
   wait             = true
 
-  yaml_body = yamlencode({
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "AppProject"
-
-    metadata = {
-      name        = var.group
-      namespace   = var.argocd_namespace
-      labels      = var.labels
-      annotations = var.annotations
-      finalizers  = var.cascade_delete ? ["resources-finalizer.argocd.argoproj.io"] : []
-    }
-    spec = {
-      roles : [
-        for permission in var.permissions : {
-          name : permission["name"],
-          description : permission["description"],
-          policies : [
-            for policy in permission["policies"] :
-            "p, proj:${var.group}:${permission["name"]}, ${policy["resource"]}, ${policy["action"]}, ${var.group}/${policy["object"]}, allow"
-          ],
-          groups : permission["groups"]
-        }
-      ]
-    }
-  })
+  yaml_body = yamlencode(try(
+    nonsensitive(local.project_manifest),
+    local.project_manifest
+  ))
 
   depends_on = [
     module.namespace
