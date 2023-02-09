31f2d1d49b030e14b158d22d2a458759114554c1
Adrian Webb <adrian@webb.sh>
2023-02-09 09:20:11 -0500

    Adding map conversion to the secrets and config data generation in the namespace submodule.

--- namespace/locals.tf
+++ namespace/locals.tf
@@ -3,7 +3,7 @@ locals {
   secrets_path = "${var.path}/secrets.yaml"
 
   secrets = fileexists(local.secrets_path) ? sensitive(
-    yamldecode(templatefile(local.secrets_path, var.variables))
+    tomap(yamldecode(templatefile(local.secrets_path, var.variables)))
   ) : {}
 
   secret_names = toset([ for name, config in local.secrets : name ])
@@ -13,7 +13,7 @@ locals {
   config_path = "${var.path}/config.yaml"
 
   config = fileexists(local.config_path) ? nonsensitive(
-    yamldecode(templatefile(local.config_path, var.variables))
+    tomap(yamldecode(templatefile(local.config_path, var.variables)))
   ) : {}
 
   config_names = toset([ for name, config in local.config : name ])
