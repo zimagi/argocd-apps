2d1f8a44d6532fe8e45e30595f3c94a7a6589fcd
Adrian Webb <adrian@webb.sh>
2023-02-09 09:36:43 -0500

    Trying a different approach to secret and config data generation.

--- namespace/locals.tf
+++ namespace/locals.tf
@@ -3,18 +3,24 @@ locals {
   secrets_path = "${var.path}/secrets.yaml"
 
   secrets = fileexists(local.secrets_path) ? sensitive(
-    tomap(yamldecode(templatefile(local.secrets_path, var.variables)))
-  ) : {}
+    yamldecode(templatefile(local.secrets_path, var.variables))
+  ) : null
 
-  secret_names = toset([ for name, config in local.secrets : name ])
+  secret_names = toset([
+    for name, config in local.secrets : name
+    if local.secrets != null
+  ])
 }
 
 locals {
   config_path = "${var.path}/config.yaml"
 
   config = fileexists(local.config_path) ? nonsensitive(
-    tomap(yamldecode(templatefile(local.config_path, var.variables)))
-  ) : {}
+    yamldecode(templatefile(local.config_path, var.variables))
+  ) : null
 
-  config_names = toset([ for name, config in local.config : name ])
+  config_names = toset([
+    for name, config in local.config : name
+    if local.config != null
+  ])
 }
