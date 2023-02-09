53784b79b586301297f05df7d01031da82f5d14a
Adrian Webb <adrian@webb.sh>
2023-02-09 09:00:15 -0500

    Fixing secrets and config local variable generation in the namespace submodule.

--- namespace/locals.tf
+++ namespace/locals.tf
@@ -2,15 +2,15 @@
 locals {
   secrets_path = "${var.path}/secrets.yaml"
 
-  secrets = fileexists(local.secrets_path) ? [
-    sensitive(templatefile(local.secrets_path, var.variables))
-  ] : {}
+  secrets = fileexists(local.secrets_path) ? sensitive(
+    yamldecode(templatefile(local.secrets_path, var.variables))
+  ) : {}
 }
 
 locals {
   config_path = "${var.path}/config.yaml"
 
-  config = fileexists(local.config_path) ? [
-    nonsensitive(templatefile(local.config_path, var.variables))
-  ] : {}
+  config = fileexists(local.config_path) ? nonsensitive(
+    yamldecode(templatefile(local.config_path, var.variables))
+  ) : {}
 }
