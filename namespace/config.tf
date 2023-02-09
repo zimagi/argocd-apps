e9a7891fba849f7fea9cb113bcad75459e241cf0
Adrian Webb <adrian@webb.sh>
2023-02-09 04:59:41 -0500

    Temporarily commenting out namespace config and secrets management.

--- namespace/config.tf
+++ namespace/config.tf
@@ -1,19 +1,19 @@
 
-resource "kubernetes_config_map" "config" {
-  count = length(local.config)
+# resource "kubernetes_config_map" "config" {
+#   count = length(local.config)
 
-  metadata {
-    name        = local.config[count.index].name
-    namespace   = var.name
-    labels      = lookup(local.config[count.index].value, "labels", {})
-    annotations = lookup(local.config[count.index].value, "annotations", {})
-  }
+#   metadata {
+#     name        = local.config[count.index].name
+#     namespace   = var.name
+#     labels      = lookup(local.config[count.index].value, "labels", {})
+#     annotations = lookup(local.config[count.index].value, "annotations", {})
+#   }
 
-  immutable   = lookup(local.config[count.index].value, "immutable", false)
-  data        = lookup(local.config[count.index].value, "data", {})
-  binary_data = lookup(local.config[count.index].value, "binary", {})
+#   immutable   = lookup(local.config[count.index].value, "immutable", false)
+#   data        = lookup(local.config[count.index].value, "data", {})
+#   binary_data = lookup(local.config[count.index].value, "binary", {})
 
-  depends_on = [
-    kubernetes_namespace.this
-  ]
-}
+#   depends_on = [
+#     kubernetes_namespace.this
+#   ]
+# }
--- namespace/secrets.tf
+++ namespace/secrets.tf
@@ -1,20 +1,20 @@
 
-resource "kubernetes_secret" "secrets" {
-  count = length(local.secrets)
+# resource "kubernetes_secret" "secrets" {
+#   count = length(local.secrets)
 
-  metadata {
-    name        = local.secrets[count.index].name
-    namespace   = var.name
-    labels      = lookup(local.secrets[count.index].value, "labels", {})
-    annotations = lookup(local.secrets[count.index].value, "annotations", {})
-  }
+#   metadata {
+#     name        = local.secrets[count.index].name
+#     namespace   = var.name
+#     labels      = lookup(local.secrets[count.index].value, "labels", {})
+#     annotations = lookup(local.secrets[count.index].value, "annotations", {})
+#   }
 
-  type        = lookup(local.secrets[count.index].value, "type", "Opaque")
-  immutable   = lookup(local.secrets[count.index].value, "immutable", false)
-  data        = sensitive(lookup(local.secrets[count.index].value, "data", {}))
-  binary_data = sensitive(lookup(local.secrets[count.index].value, "binary", {}))
+#   type        = lookup(local.secrets[count.index].value, "type", "Opaque")
+#   immutable   = lookup(local.secrets[count.index].value, "immutable", false)
+#   data        = sensitive(lookup(local.secrets[count.index].value, "data", {}))
+#   binary_data = sensitive(lookup(local.secrets[count.index].value, "binary", {}))
 
-  depends_on = [
-    kubernetes_namespace.this
-  ]
-}
+#   depends_on = [
+#     kubernetes_namespace.this
+#   ]
+# }
