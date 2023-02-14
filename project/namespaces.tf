008343ff1ac101d01708f8d0a5232787825273fb
Adrian Webb <adrian@webb.sh>
2023-02-14 05:03:20 -0500

    Fixing namespace access and default in the project namespaces manifest.

--- project/namespaces.tf
+++ project/namespaces.tf
@@ -2,9 +2,9 @@
 module "namespace" {
   for_each = local.applications
 
-  source      = "../namespace"
+  source = "../namespace"
 
-  name        = each.value.name
-  path        = each.key
-  variables   = var.variables
+  name      = lookup(each.value, "namespace", each.value.name)
+  path      = each.key
+  variables = var.variables
 }
