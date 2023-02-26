bf8d2c848f353faec9d4a8fed4027388514f74bd
Adrian Webb <adrian@webb.sh>
2023-02-26 06:26:23 -0500

    Removing the variable interpolation from generation of configmap and secret names in the namespace submodule.

--- namespace/locals.tf
+++ namespace/locals.tf
@@ -2,23 +2,31 @@
 locals {
   secrets_path = "${var.path}/secrets.yaml"
 
+  secrets_raw = fileexists(local.secrets_path) ? sensitive(
+    yamldecode(file(local.secrets_path))
+  ) : null
+
   secrets = fileexists(local.secrets_path) ? sensitive(
     yamldecode(templatefile(local.secrets_path, var.variables))
   ) : null
 
-  secret_names = toset(local.secrets != null ? [
-    for name, config in local.secrets : name
+  secret_names = toset(local.secrets_raw != null ? [
+    for name, config in local.secrets_raw : name
   ] : [])
 }
 
 locals {
   config_path = "${var.path}/config.yaml"
 
+  config_raw = fileexists(local.config_path) ? nonsensitive(
+    yamldecode(file(local.config_path))
+  ) : null
+
   config = fileexists(local.config_path) ? nonsensitive(
     yamldecode(templatefile(local.config_path, var.variables))
   ) : null
 
-  config_names = toset(local.config != null ? [
-    for name, config in local.config : name
+  config_names = toset(local.config_raw != null ? [
+    for name, config in local.config_raw : name
   ] : [])
 }
