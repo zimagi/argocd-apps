cea4f4982927b06b96411b64a3c10e14443b1c15
Adrian Webb <adrian@webb.sh>
2023-03-20 20:53:02 -0400

    Updating the default application chart version.

--- variables.tf
+++ variables.tf
@@ -65,5 +65,5 @@ variable "default_path" {
 variable "default_version" {
   type        = string
   description = "Default ArgoCD application Helm chart version from Helm repository"
-  default     = "1.1.1"
+  default     = "1.1.2"
 }
