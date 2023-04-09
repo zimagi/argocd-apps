9528729d293efb22d8c4bcc3b5cc179f9d2c3c59
Adrian Webb <adrian@webb.sh>
2023-04-09 03:33:44 -0400

    Adding application and config_path variable values for namespaces in the project submodule.

--- project/namespaces.tf
+++ project/namespaces.tf
@@ -7,8 +7,11 @@ module "namespace" {
   name   = lookup(each.value, "namespace", each.value.name)
   exists = lookup(each.value, "namespace_exists", false)
 
-  path      = each.key
-  variables = var.variables
+  application = each.value["name"]
+
+  path        = each.key
+  config_path = var.config_path
+  variables   = var.variables
 
   labels      = merge(var.labels, lookup(each.value, "labels", {}))
   annotations = merge(var.annotations, lookup(each.value, "annotations", {}))
