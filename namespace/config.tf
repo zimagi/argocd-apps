cf149301ae896661f4ed1b4b57a42e333cbd42ae
Adrian Webb <adrian@webb.sh>
2023-04-09 03:32:30 -0400

    Updating the secrets and config terraform manifests in the namespace submodule to merge core and extra configurations and secrets.

--- namespace/config.tf
+++ namespace/config.tf
@@ -5,13 +5,30 @@ resource "kubernetes_config_map" "config" {
   metadata {
     name        = each.value
     namespace   = var.name
-    labels      = lookup(local.config[each.value], "labels", {})
-    annotations = lookup(local.config[each.value], "annotations", {})
+
+    labels = merge(
+      local.config != null ? (lookup(local.config, each.value, null) != null ? lookup(local.config[each.value], "labels", null) : null) : null,
+      local.extra_config != null ? (lookup(local.extra_config, each.value, null) != null ? lookup(local.extra_config[each.value], "labels", null) : null) : null
+    )
+    annotations = merge(
+      local.config != null ? (lookup(local.config, each.value, null) != null ? lookup(local.config[each.value], "annotations", null) : null) : null,
+      local.extra_config != null ? (lookup(local.extra_config, each.value, null) != null ? lookup(local.extra_config[each.value], "annotations", null) : null) : null
+    )
   }
 
-  immutable   = lookup(local.config[each.value], "immutable", false)
-  data        = lookup(local.config[each.value], "data", {})
-  binary_data = lookup(local.config[each.value], "binary", {})
+  immutable = coalesce(
+    local.config != null ? (lookup(local.config, each.value, null) != null ? lookup(local.config[each.value], "immutable", null) : null) : null,
+    local.extra_config != null ? (lookup(local.extra_config, each.value, null) != null ? lookup(local.extra_config[each.value], "immutable", false) : false) : false
+  )
+
+  data = merge(
+    local.config != null ? (lookup(local.config, each.value, null) != null ? lookup(local.config[each.value], "data", null) : null) : null,
+    local.extra_config != null ? (lookup(local.extra_config, each.value, null) != null ? lookup(local.extra_config[each.value], "data", null) : null) : null
+  )
+  binary_data = merge(
+    local.config != null ? (lookup(local.config, each.value, null) != null ? lookup(local.config[each.value], "binary", null) : null) : null,
+    local.extra_config != null ? (lookup(local.extra_config, each.value, null) != null ? lookup(local.extra_config[each.value], "binary", null) : null) : null
+  )
 
   depends_on = [
     kubernetes_namespace.this
--- namespace/secrets.tf
+++ namespace/secrets.tf
@@ -3,16 +3,36 @@ resource "kubernetes_secret" "secrets" {
   for_each = try(nonsensitive(local.secret_names), local.secret_names)
 
   metadata {
-    name        = each.value
-    namespace   = var.name
-    labels      = lookup(local.secrets[each.value], "labels", {})
-    annotations = lookup(local.secrets[each.value], "annotations", {})
+    name      = each.value
+    namespace = var.name
+
+    labels = merge(
+      local.secrets != null ? (lookup(local.secrets, each.value, null) != null ? lookup(local.secrets[each.value], "labels", null) : null) : null,
+      local.extra_secrets != null ? (lookup(local.extra_secrets, each.value, null) != null ? lookup(local.extra_secrets[each.value], "labels", null) : null) : null
+    )
+    annotations = merge(
+      local.secrets != null ? (lookup(local.secrets, each.value, null) != null ? lookup(local.secrets[each.value], "annotations", null) : null) : null,
+      local.extra_secrets != null ? (lookup(local.extra_secrets, each.value, null) != null ? lookup(local.extra_secrets[each.value], "annotations", null) : null) : null
+    )
   }
 
-  type        = lookup(local.secrets[each.value], "type", "Opaque")
-  immutable   = lookup(local.secrets[each.value], "immutable", false)
-  data        = sensitive(lookup(local.secrets[each.value], "data", {}))
-  binary_data = sensitive(lookup(local.secrets[each.value], "binary", {}))
+  type = coalesce(
+    local.secrets != null ? (lookup(local.secrets, each.value, null) != null ? lookup(local.secrets[each.value], "type", null) : null) : null,
+    local.extra_secrets != null ? (lookup(local.extra_secrets, each.value, null) != null ? lookup(local.extra_secrets[each.value], "type", "Opaque") : "Opaque") : "Opaque"
+  )
+  immutable = coalesce(
+    local.secrets != null ? (lookup(local.secrets, each.value, null) != null ? lookup(local.secrets[each.value], "immutable", null) : null) : null,
+    local.extra_secrets != null ? (lookup(local.extra_secrets, each.value, null) != null ? lookup(local.extra_secrets[each.value], "immutable", false) : false) : false
+  )
+
+  data = sensitive(merge(
+    local.secrets != null ? (lookup(local.secrets, each.value, null) != null ? lookup(local.secrets[each.value], "data", null) : null) : null,
+    local.extra_secrets != null ? (lookup(local.extra_secrets, each.value, null) != null ? lookup(local.extra_secrets[each.value], "data", null) : null) : null
+  ))
+  binary_data = sensitive(merge(
+    local.secrets != null ? (lookup(local.secrets, each.value, null) != null ? lookup(local.secrets[each.value], "binary", null) : null) : null,
+    local.extra_secrets != null ? (lookup(local.extra_secrets, each.value, null) != null ? lookup(local.extra_secrets[each.value], "binary", null) : null) : null
+  ))
 
   depends_on = [
     kubernetes_namespace.this
